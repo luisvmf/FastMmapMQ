a6c6ffbc5c88e5347c525001f791c13c0141ebb2
luisvmf <luis@luisvmf.com>
2022-09-13 15:30:44 -0300

    Fix memory leak

--- receive_example.c
+++ receive_example.c
@@ -13,9 +13,10 @@ char *receive;
 printf("Waiting for message from ./send_example...\n");
 receive=readmmap(mapid,1);
 while(receive[0]=='\0'){
+	free(receive);
 	receive=readmmap(mapid,1);
 	sleep(0.1);
 }
 printf("Received message from ./send_example: %s\n",receive);
-
+free(receive);
 }
